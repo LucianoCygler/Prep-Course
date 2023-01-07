560609cff801fa4186a45872bf2947a586e61138
Luciano <lucianocygler@outlook.com>
2023-01-07 06:49:00 -0300

    subo

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -260,13 +260,24 @@ function tablaDelSeis() {
 function tieneTresDigitos(numero) {
   //Leer un número entero y retornar true si tiene 3 dígitos. Caso contrario, retorna false.
   //Escribe tu código aquí
-
+ var long=  numero.toString().length;
+ if(long ==3){
+  return true;
+ }
+ else{
+  return false;
+ }
 }
 
 function doWhile(numero) {
   //Implementar una función tal que vaya aumentando el valor recibido en 5 hasta un límite de 8 veces
   //Retornar el valor final.
   //Usar el bucle do ... while.
+  var i = 0;
+  do {
+    numero+=5;
+    i++;
+  } while (i<7);
 }
 
 
